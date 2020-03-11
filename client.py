9a9a51629a5321f5ead7a042cd659c0aa22c698f
Louis-Gv <56234907+Louis-Gv@users.noreply.github.com>
2020-03-11 13:41:31 +0100

    Ajout échange v3.0

--- client.py
+++ client.py
@@ -2,14 +2,14 @@
 
 import socket
 
-hote = "127.0.0.1"
+hote = "localhost"
 port = 15555
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((hote, port))
 print("Connection on {}".format(port))
 
-socket.send("Hey my name is Olivier!")
+socket.send("Hey my name is Olivier!".encode())
 
 print("Close")
 socket.close()
