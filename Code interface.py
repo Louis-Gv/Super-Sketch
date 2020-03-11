07c958c459ab207240955284eeb979c237c2ff41
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2020-03-11 16:40:45 +0100

    Update Code interface.py

--- Code interface.py
+++ Code interface.py
@@ -1,1 +1,25 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+#Ouverture de la fenêtre Pygame
+fenetre = pygame.display.set_mode((1920, 1080), FULLSCREEN)
+
+
+
+
+
+
+
+
+
+#Boucle infinie
+continuer = 1
+while continuer:
+        for event in pygame.event.get():   
+                if event.type == QUIT:    
+                    pygame.quit()
+                    continuer =0
+
+
 
