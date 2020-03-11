16f6ced390808ef03dfff509b080f99aa597832c
Louis-Gv <56234907+Louis-Gv@users.noreply.github.com>
2020-03-11 16:52:50 +0100

    Aproprissement

--- client.py
+++ client.py
@@ -1,15 +1,23 @@
-# coding: utf-8
-
 import socket
-
-hote = "localhost"
-port = 15555
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((hote, port))
-print("Connection on {}".format(port))
-
-socket.send("Hey my name is Olivier!".encode())
-
-print("Close")
-socket.close()
+ 
+def Main():
+        host = '127.0.0.1'
+        port = 5000
+         
+        mySocket = socket.socket()
+        mySocket.connect((host,port))
+         
+        message = input(" -> ")
+         
+        while message != 'q':
+                mySocket.send(message.encode())
+                data = mySocket.recv(1024).decode()
+                 
+                print ('Received from server: ' + data)
+                 
+                message = input(" -> ")
+                 
+        mySocket.close()
+ 
+if __name__ == '__main__':
+    Main()
