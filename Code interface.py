653bb08240e8278e5d953d8c2ea358fca3be3fb8
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2020-03-11 18:03:31 +0100

    button

--- Code interface.py
+++ Code interface.py
@@ -1,13 +1,15 @@
 import pygame
 from pygame.locals import *
-coucou
+
 pygame.init()
 #Ouverture de la fenêtre Pygame
 fenetre = pygame.display.set_mode((1920, 1080), FULLSCREEN)
+pygame.display.set_caption("Super-sketch")
 
-
-
-
+        
+Bouton_1 = pygame.image.load("img/bouton.png").convert_alpha()
+Bouton_1_pos = (100, 100)
+fenetre.blit(Bouton_1, Bouton_1_pos)
 
 
 
@@ -20,6 +22,11 @@ while continuer:
                 if event.type == QUIT:    
                     pygame.quit()
                     continuer =0
+        pygame.display.flip()
+
+
+
+        
 
 
 
