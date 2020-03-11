a2212e1e91f12211d491696f366a108d971548a0
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2020-03-11 19:31:44 +0100

    à changer et simplifier

--- Code interface.py
+++ Code interface.py
@@ -5,9 +5,17 @@ pygame.init()
 #Ouverture de la fenêtre Pygame en plein écran
 fenetre = pygame.display.set_mode((1920, 1080), FULLSCREEN)
 pygame.display.set_caption("Super-sketch")
+clock = pygame.time.Clock()
 
+pygame.init()
 fond = pygame.image.load("img/fond.png").convert()
 fenetre.blit(fond, (0,0))       
+fond2 = pygame.image.load("img/fond2.png").convert()
+size = width, height = (100,100)
+canvas = pygame.Surface(size)
+
+
+
 #Chargement et collage du bouton bleu clair
 btbleu = pygame.image.load("img/bleuc.png").convert_alpha()
 btbleu_pos = (1675, 200)
@@ -68,17 +76,38 @@ btdown = pygame.image.load("img/down.png").convert_alpha()
 btdown_pos = (1625,570)
 fenetre.blit(btdown, btdown_pos)
 
+
+
+press = False
+
 #Boucle infinie pour maintenir ou fermer la fenêtre
 continuer = 1
 while continuer:
-        for event in pygame.event.get():   
-                if event.type == QUIT:    
-                    pygame.quit()
-                    continuer =0
-        pygame.display.flip()
+        try:
+                fenetre.blit(canvas,(400,400))
+                pygame.init()
+                for event in pygame.event.get():                       
+                        if event.type == QUIT:                                                             
+                                pygame.quit()
+                                continuer =0
+
+                pygame.init()
+                px, py = pygame.mouse.get_pos()
+                if pygame.mouse.get_pressed() == (1,0,0):
+                        pygame.draw.rect(fenetre, (128,128,128), (px,py,10,10))
+ 
+                if event.type == pygame.MOUSEBUTTONUP:                        
+                        press == False
+                pygame.display.update()
+                clock.tick(1000)
+                
+        
+        except Exception as e:
+            print(e)
+            pygame.quit()
 
 
-
+pygame.quit()
         
 
 
