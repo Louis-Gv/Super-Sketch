49f0591e6696afb3050953e0e2ce9826c69450c2
Louis-Gv <56234907+Louis-Gv@users.noreply.github.com>
2020-03-11 17:33:56 +0100

    bouclage serveur

--- client.py
+++ client.py
@@ -1,23 +1,19 @@
 import socket
- 
-def Main():
-        host = '127.0.0.1'
-        port = 5000
-         
-        mySocket = socket.socket()
-        mySocket.connect((host,port))
-         
-        message = input(" -> ")
-         
-        while message != 'q':
-                mySocket.send(message.encode())
-                data = mySocket.recv(1024).decode()
-                 
-                print ('Received from server: ' + data)
-                 
-                message = input(" -> ")
-                 
-        mySocket.close()
- 
-if __name__ == '__main__':
-    Main()
+
+host = 'localhost'
+port = 5000
+
+monSocket = socket.socket()
+monSocket.connect((host, port))
+
+message = input(" -> ")
+
+while message != 'q':
+    monSocket.send(message.encode())
+    data = monSocket.recv(1024).decode()
+
+    print('Received from server: ' + data)
+
+    message = input(" -> ")
+
+monSocket.close()
