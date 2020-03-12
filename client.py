9728e65d915caf36aaa156a540c8938f22d8dbf3
Louis-Gv <56234907+Louis-Gv@users.noreply.github.com>
2020-03-12 21:31:52 +0100

    Découverte d'hote du turfu

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
-import socket
+from socket import *
+import time
 
-host = 'localhost'
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(('', 50000))
+
+timeout = time.time() + 10
+
+while 1:
+    data, wherefrom = s.recvfrom(1500, 0)  # (b'du texte', ('192.168.1.32', 62587))
+    if data.decode() == "SuperS":
+        print(wherefrom[0])
+        break
+    # if time.time() > timeout:
+    #   break
+
+
 port = 5000
 
-monSocket = socket.socket()
-monSocket.connect((host, port))
+monSocket = socket()
+monSocket.connect((wherefrom[0], port))  # On se conncecte à l'ip du stream
 
 message = input(" -> ")
 
