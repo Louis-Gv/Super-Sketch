7495c8f01fc3edc707d8a5d43746749e0297ba54
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2020-03-25 13:44:06 +0100

    Update SuperSketch.py

--- SuperSketch.py
+++ SuperSketch.py
@@ -410,7 +410,7 @@ if __name__ == '__main__':  # Si c'est le programme pricipal / obligatoire pour 
 
                 if pygame.mouse.get_pressed()[0] == 1 and (px != ancienpx or py != ancienpy):
                     pygame.draw.circle(fenetre, couleur, (px, py), rayon)
-                    tunnelParent.send(('D,' + str(px) + "," + str(py)+","+str(couleur[0])+";"+str(couleur[1])+";"+str(couleur[2])+","+str(rayon)).encode())
+                    tunnelParent.send(('D,' + str(px) + "," + str(py)+","+str(couleur[0])+";"+str(couleur[1])+";"+str(couleur[2])+","+str(rayon) + ",").encode())
                     # "D,875,745,45;75;0,10"
                     ancienpx = px
                     ancienpy = py
